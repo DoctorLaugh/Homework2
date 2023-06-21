81938a21af3b4e210c369499b42b44b4c247283d
Dmitriy <trayan@yandex.ru>
2023-06-21 13:21:15 +1100

    Task15 added

---
+++ Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер дня недели");
+int wd = int.Parse(Console.ReadLine());
+if(wd>7 || wd<1)
+{
+    Console.WriteLine("Нет такого дня недели");
+    return;
+}
+if(wd==6 || wd==7)
+{
+    Console.WriteLine("Выходной день");
+}
+else
+{
+    Console.WriteLine("Рабочий день");
+}
