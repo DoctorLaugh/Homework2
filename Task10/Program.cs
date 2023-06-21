51a4ca087beda776073ed577b193d3f85347537e
Dmitriy <trayan@yandex.ru>
2023-06-21 13:01:04 +1100

    Task10 added

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трёхзначное число");
+int num = int.Parse(Console.ReadLine());
+if(num<100 || num>999)
+{
+    Console.WriteLine("Ошибка ввода");
+    return;
+}
+int num1 = num/10;
+int num2 = num1%10;
+Console.WriteLine($"Вторая цифра числа {num} - {num2}");
