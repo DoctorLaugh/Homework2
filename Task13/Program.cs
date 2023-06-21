4c37c9fb279e534a3204d7485b92866580b4b888
Dmitriy <trayan@yandex.ru>
2023-06-21 13:12:41 +1100

    Task13 added

---
+++ Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+if(num<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+    return;
+}
+while(num>999)
+{
+    num = num/10;
+}
+int res = num%10;
+Console.WriteLine($"Третья цифра - {res}");
